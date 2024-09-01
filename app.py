55e388bf42e89c5b34ef0f0aa1327bf23aa7bd65
chrisZerv <166018143+chrisZerv@users.noreply.github.com>
2024-09-01 15:02:44 +0300

    Initial code.

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean
+
+app = Flask(__name__)
+
+
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CREATE TABLE
+class Cafe(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    map_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    location: Mapped[str] = mapped_column(String(250), nullable=False)
+    seats: Mapped[str] = mapped_column(String(250), nullable=False)
+    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
+    all_cafes = result.scalars().all()
+    return render_template("index.html", cafes=all_cafes)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_cafe():
+    if request.method == "POST":
+        new_cafe = Cafe(
+            name=request.form["name"],
+            map_url=request.form["map_url"],
+            img_url=request.form["img_url"],
+            location=request.form["location"],
+            has_sockets=bool(request.form.get("sockets")),
+            has_toilet=bool(request.form.get("toilet")),
+            has_wifi=bool(request.form.get("wifi")),
+            can_take_calls=bool(request.form.get("calls")),
+            seats=request.form["seats"],
+            coffee_price=request.form["coffee_price"],
+        )
+        db.session.add(new_cafe)
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template("add_cafe.html")
+
+
+@app.route("/delete/<int:cafe_id>", methods=["POST"])
+def delete_cafe(cafe_id):
+    cafe = db.get_or_404(Cafe, cafe_id)
+    db.session.delete(cafe)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
